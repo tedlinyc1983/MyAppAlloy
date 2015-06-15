61e07a664e58387c88b787785d3e13b9106fbe64
ted <ted@teds-MacBook-Pro.local>
2015-06-15 11:47:13 +0800

    first commit

---
+++ app/controllers/index.js
@@ -0,0 +1,11 @@
+function clickImage(e) {
+    Titanium.API.info("clickImage: ");	
+    Titanium.UI.createAlertDialog({title:'Image View', message:'You clicked me!'}).show();
+}
+$.index.open();
+
+//function doClick(e) {
+//    alert($.label.text);
+//}
+//
+//$.index.open();
